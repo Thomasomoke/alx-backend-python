3c2a6053acd898e2a869c5d23ac86f2ed3ced4b0
Thomasomoke <omokethomas644@gmail.com>
2024-11-04 19:28:11 +0000

    More patching

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 
 """
 Unit tests for the client module.
-Tests GithubOrgClient functionality
+Tests GithubOrgClient functionality, focusing on public repositories.
 """
 
 import unittest
@@ -11,22 +11,31 @@ from client import GithubOrgClient
 
 
 class TestGithubOrgClient(unittest.TestCase):
-    """Test suite for GithubOrgClient class.
-    Verifies that the _public_repos_url method returns the expected URL.
-    """
-    @patch('client.GithubOrgClient.org')
-    def test_public_repos_url(self, mock_org):
-        """Tests that _public_repos_url returns the
-        expected URL based on the mocked org.
-        """
-        mock_org.return_value = {
-            "repos_url": "https://api.github.com/orgs/example/repos"
-            }
-        client = GithubOrgClient("example")
-        result = client._public_repos_url()
+    """Test suite for GithubOrgClient class."""
 
-        self.assertEqual(result, "https://api.github.com/orgs/example/repos")
-        mock_org.assert_called_once()
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json):
+        """Tests that public_repos returns the expected
+        list of repositories."""
+        mock_get_json.return_value = [
+            {"name": "repo1", "url":
+                "https://api.github.com/repos/example/repo1"},
+            {"name": "repo2", "url":
+                "https://api.github.com/repos/example/repo2"},
+        ]
+
+        with patch
+        ('client.GithubOrgClient._public_repos_url', new_callable=property)
+        as mock_public_repos_url:
+            mock_public_repos_url.return_value =
+            "https://api.github.com/orgs/example/repos"
+            client = GithubOrgClient("example")
+            repos = client.public_repos()
+            expected_repos = ["repo1", "repo2"]
+            self.assertEqual(repos, expected_repos)
+            mock_public_repos_url.assert_called_once()
+            mock_get_json.assert_called_once_with
+            ("https://api.github.com/orgs/example/repos")
 
 
 if __name__ == "__main__":
